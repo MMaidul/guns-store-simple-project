2c74bc0dbc9baa1b6fa0a05e79a4636669f6e659
MMaidul <maidulmuqtadir360@gmail.com>
2022-03-24 14:16:24 +0600

    data fetech and map with display show card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Guns from './componets/Guns/Guns';
 
 function App() {
+  const [guns, setGuns] = useState([]);
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => setGuns(data))
+  }, [])
+
   return (
     <div className='guns-container'>
-      <Guns></Guns>
-      <Guns></Guns>
-      <Guns></Guns>
-      <Guns></Guns>
+      {
+        guns.map(gun => <Guns />)
+      }
     </div>
   );
 }
